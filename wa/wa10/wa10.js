92d34745bc0459f7c59f7563ad27f1b88070afda
OliviaSavino <116123152+OliviaSavino@users.noreply.github.com>
2024-03-21 18:26:00 -0600

    wa10

---
+++ wa/wa10/wa10.js
@@ -0,0 +1,55 @@
+const customName = document.getElementById("customname");
+const randomize = document.querySelector(".randomize");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+const storyText =
+  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = [
+  "the Art Institute of Chicago",
+  "Millennium Park",
+  "the Magnificent Mile",
+];
+const insertZ = [
+  "started melting under the scorching sun",
+  "suddenly burst into flames",
+  "disintegrated into a puddle of sweat",
+];
+
+randomize.addEventListener("click", result);
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(":insertx:", xItem);
+  newStory = newStory.replaceAll(":inserty:", yItem);
+  newStory = newStory.replaceAll(":insertz:", zItem);
+
+  if (!customName.value) {
+    story.style.visibility = "hidden";
+    alert("Please enter a name");
+    return;
+  }
+
+  const name = customName.value;
+  newStory = newStory.replaceAll("Bob", name);
+
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300 * 0.0714286)} stone`;
+    const temperature = `${Math.round(((94 - 32) * 5) / 9)} centigrade`;
+    newStory = newStory.replaceAll("94 fahrenheit", temperature);
+    newStory = newStory.replaceAll("300 pounds", weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
